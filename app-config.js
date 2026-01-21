23d606e69180ce895d25899cb707101e339907be
JeryPSK <ap-pskov@ya.ru>
2026-01-22 00:46:25 +0300

    Initial commit

---
+++ app-config.js
@@ -0,0 +1,14 @@
+const appConfig = {
+    baseFormat: ['twig', 'scss'],
+    ts: true,
+    pathFonts: {
+        ttf: "public/fonts/ttf/",
+        woff: "public/fonts/",
+        fileStyle: "src/styles/_fonts.scss",
+    },
+    pathWget: "public/wget",
+    sections: "src/sections",
+    components: "src/components",
+};
+
+export default appConfig;
